2cd0ff1d2091ad76d9cda5d5a2c210ce01f86c83
cho leong zhuang <lol1902192423@gmail.com>
2026-04-03 21:40:57 +0800

    A new homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./app/javascript/**/*.js",
     "./app/helpers/**/*.rb",  
   ],
-  theme: { extend: {} },
+  theme: { 
+      extend: {
+        fontFamily: {
+        // 'custom' 是你自己起的類名，'Noto Sans TC' 必須與 Google Fonts 提供的一致
+        'custom': ['"Saira Stencil One"','sans-serif'],
+        },
+    } 
+  },
   plugins: [],
 }
