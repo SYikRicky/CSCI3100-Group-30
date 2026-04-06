d5358bc69f7a1b38121513d9731cf77938592b60
cho leong zhuang <lol1902192423@gmail.com>
2026-04-06 20:59:22 +0800

    fixing issue with authorization

--- spec/requests/stocks_spec.rb
+++ spec/requests/stocks_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Stocks", type: :request do
+  let(:user) { create(:user) }
+  before { sign_in user }
   describe "GET /index" do
     it "returns http success" do
       get "/stocks/index"
